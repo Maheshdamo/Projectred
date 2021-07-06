b76d76a1ce4e16e865a76c1127446b1353323c61
Maheshdamo <Maheshmagi1057@gmail.com>
2021-07-06 02:21:04 -0700

    work

---
+++ src/org/employee/Employee.java
@@ -0,0 +1,21 @@
+package org.employee;
+
+public class Employee {
+	public static void main(String[] args) {
+		int empId = 12345;
+				System.out.println("empid is "+empId);
+				String empName = "mahesh";
+				System.out.println("empname is "+empName);
+	String empEmail = "maheshhunt@gmail.com";
+	System.out.println("emp Email is "+empEmail);
+	long empPhone = 8056121063l;
+	System.out.println("empphone is "+empPhone);
+	float empSalary = 18000.00f;
+	System.out.println("emp salary is "+empSalary);
+	char empGender = 'm';
+	System.out.println("empgender is "+empGender);
+	String empCity = "Chennai";
+	System.out.println("empCity is "+empCity);
+	}
+
+}
