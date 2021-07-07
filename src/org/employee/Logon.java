0f5b3930201bf9db616894a1279197c950139634
Maheshdamo <Maheshmagi1057@gmail.com>
2021-07-06 23:03:15 -0700

    swetha commit

---
+++ src/org/employee/Logon.java
@@ -0,0 +1,14 @@
+package org.employee;
+
+public class Logon {
+	
+private void password() {
+	
+System.out.println("password");
+}
+
+public static void main(String[] args) {
+	Logon us = new Logon();
+	us.password();
+}
+}
