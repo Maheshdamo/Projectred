b266f1454c919df61352dda8edf5081ccca4feb4
Maheshdamo <Maheshmagi1057@gmail.com>
2021-07-06 22:56:16 -0700

    yesh commit

---
+++ src/org/employee/Logon.java
@@ -0,0 +1,16 @@
+package org.employee;
+
+public class Logon {
+	
+	private void Username() {
+System.out.println("Username");
+
+	}
+	public static void main(String[] args) {
+		Logon us = new Logon();
+		us.Username();
+		
+	}
+	
+
+}
