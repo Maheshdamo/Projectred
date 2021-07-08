61523c8b83e4d1c38c06831e1354e4621b67b983
Maheshdamo <Maheshmagi1057@gmail.com>
2021-07-08 07:53:27 -0700

    tokio commit

---
+++ src/org/employee/Witcher.java
@@ -0,0 +1,13 @@
+package org.employee;
+
+public class Witcher {
+	private void emp() {
+		
+System.out.println("yt");
+	}
+	public static void main(String[] args) {
+		Witcher ed = new Witcher();
+		ed.emp();
+	}
+
+}
